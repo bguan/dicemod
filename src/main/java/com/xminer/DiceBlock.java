3040f32d3574cdeda5b911b4d232bb5cf1561ba7
bguan <brian.guan@gmail.com>
2021-02-24 18:57:12 -0800

    Fully implemented Dice block. 6 variants by face1 direction, then 4 horizontal face random picked. Random face up on place and activate. On harvest go back to inventory. On game start auto add to inventory even in survival.

--- src/main/java/com/xminer/DiceBlock.java
+++ src/main/java/com/xminer/DiceBlock.java
@@ -1,15 +1,81 @@
 package com.xminer;
 
+import java.util.Random;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber
 public class DiceBlock extends Block {
 
-	public static final String BLOCKID = "diceblock";
-	
+	public static final String BLOCK_ID = "diceblock";
+
+	private static final Logger LOGGER = LogManager.getLogger();
+
+	public static final DirectionProperty FACING = BlockStateProperties.FACING;
+
+	private static final Random RANDOM = new Random();
+
+    @SubscribeEvent
+    public static void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event)
+    {
+		LOGGER.info("Gift for player");
+        event.getPlayer().inventory.addItemStackToInventory(new ItemStack(DiceBlockMod.itemDiceBlock));
+    }
+    
 	public DiceBlock() {
-		super(Block.Properties.create(Material.ROCK).hardnessAndResistance(10, 1).sound(SoundType.BONE));
+		super(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1).sound(SoundType.BONE));
+		this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.UP));
+	}
+
+	@Override
+	public BlockState getStateForPlacement(BlockItemUseContext context) {
+		Direction d = Direction.getRandomDirection(RANDOM);
+		LOGGER.info("Random Dice Roll Face: " + d);
+		return getDefaultState().with(FACING, d);
+	}
+
+	@Override
+	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+		super.fillStateContainer(builder);
+		builder.add(FACING);
+	}
+
+	@Override
+	public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player,
+			Hand handIn, BlockRayTraceResult hit) {
+		worldIn.playSound(player, pos, SoundEvents.BLOCK_BONE_BLOCK_PLACE, SoundCategory.BLOCKS, jumpFactor,
+				blastResistance);
+		worldIn.setBlockState(pos, state.with(FACING, Direction.getRandomDirection(RANDOM)));
+		return ActionResultType.SUCCESS;
+	}
+
+	@Override
+	public void onBlockHarvested(World worldIn, BlockPos pos, BlockState state, PlayerEntity player) {
+        player.inventory.addItemStackToInventory(new ItemStack(state.getBlock().asItem()));
+		super.onBlockHarvested(worldIn, pos, state, player);
 	}
 
 }
--- src/main/java/com/xminer/DiceBlockMod.java
+++ src/main/java/com/xminer/DiceBlockMod.java
@@ -1,5 +1,8 @@
 package com.xminer;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
@@ -13,23 +16,13 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod(DiceBlockMod.MODID)
+@Mod(DiceBlockMod.MOD_ID)
 public class DiceBlockMod {
-	public static final String MODID = "diceblockmod";
-	
-	// Directly reference a log4j logger.
 	private static final Logger LOGGER = LogManager.getLogger();
-
-	// get a reference to the event bus for this mod; Registration events are fired
-	// on this bus.
+	public static final String MOD_ID = "diceblockmod";
 	public static IEventBus MOD_EVENT_BUS;
-
 	public static DiceBlock diceBlock;
 	public static BlockItem itemDiceBlock;
 
@@ -54,28 +47,19 @@ public class DiceBlockMod {
 	}
 
 	@SubscribeEvent
-	public void onServerStarting(FMLServerStartingEvent event) {
-		// do something when the server starts
-		LOGGER.info("HELLO from server starting");
-	}
-
-	@SubscribeEvent
 	public static void onBlocksRegistration(final RegistryEvent.Register<Block> blockRegisterEvent) {
-		diceBlock = (DiceBlock) (new DiceBlock().setRegistryName(MODID, DiceBlock.BLOCKID));
+		LOGGER.info("Registering Dice Block");
+		diceBlock = (DiceBlock) (new DiceBlock().setRegistryName(MOD_ID, DiceBlock.BLOCK_ID));
 		blockRegisterEvent.getRegistry().register(diceBlock);
 	}
 
 	@SubscribeEvent
 	public static void onItemsRegistration(final RegistryEvent.Register<Item> itemRegisterEvent) {
-		// We need to create a BlockItem so the player can carry this block in their
-		// hand and it can appear in the inventory
-		final int MAXIMUM_STACK_SIZE = 1; // player can only hold 1 dice block in their hand at once
-
-		Item.Properties itemSimpleProperties = new Item.Properties()
-				.maxStackSize(MAXIMUM_STACK_SIZE)
-				.group(ItemGroup.BUILDING_BLOCKS); // which inventory tab?
-		itemDiceBlock = new BlockItem(diceBlock, itemSimpleProperties);
+		LOGGER.info("Registering Dice Item");
+		Item.Properties itemProps = new Item.Properties().maxStackSize(999).group(ItemGroup.MISC);
+		itemDiceBlock = new BlockItem(diceBlock, itemProps);
 		itemDiceBlock.setRegistryName(diceBlock.getRegistryName());
 		itemRegisterEvent.getRegistry().register(itemDiceBlock);
 	}
+	
 }
